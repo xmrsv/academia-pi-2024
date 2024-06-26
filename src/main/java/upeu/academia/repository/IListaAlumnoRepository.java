46d5a28beb28b9ddf78f76a2c07dcd5047241bf4
xmrsv <88881293+xmrsv@users.noreply.github.com>
2024-06-26 15:12:15 -0500

    Formato y comentarios

--- src/main/java/upeu/academia/repository/IListaAlumnoRepository.java
+++ src/main/java/upeu/academia/repository/IListaAlumnoRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import upeu.academia.domain.entity.ListaAlumno;
 
+/**
+ * 
+ * @author Jerika Reyna
+ */
 public interface IListaAlumnoRepository extends JpaRepository<ListaAlumno, Integer> {
 
-    
 }
